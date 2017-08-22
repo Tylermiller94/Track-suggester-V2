903471aa1df663c5641bf05602fcdcfb991c58ae
tylermiller94 <tylermiller94@gmail.com>
2017-08-21 22:20:39 -0700

    add Jquery, quiz functionality, base layout for styling

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $(".questions").submit(function(event){
+    var q1input = parseInt($("input:radio[name=q1]:checked").val());
+    var q2input = parseInt($("input:radio[name=q2]:checked").val());
+    var q3input = parseInt($("input:radio[name=q3]:checked").val());
+    var q4input = parseInt($("input:radio[name=q4]:checked").val());
+    var q5input = parseInt($("input:radio[name=q5]:checked").val());
+    var result = q1input + q2input + q3input + q4input + q5input;
+
+    if (result <= 5){
+      $("#csharp").show();
+      $("#css").hide();
+      $("#ruby").hide();
+    } else if (result > 5 && result <= 10){
+      $("#csharp").hide();
+      $("#css").show();
+      $("#ruby").hide();
+    } else if (result > 10){
+      $("#csharp").hide();
+      $("#java").hide();
+      $("#ruby").show();
+
+}
+       event.preventDefault();
+  });
+ });
